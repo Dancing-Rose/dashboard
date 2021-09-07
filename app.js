6ceb14d954d47016cc590d76e572276435151b3c
Sharvin1106 <sharvin.kogilavanan@gmail.com>
2021-09-07 20:58:45 +0800

    Adding storage feature

--- app.js
+++ app.js
@@ -1,12 +1,75 @@
 const express = require("express");
+const multer = require("multer");
+const ejs = require("ejs");
+const path = require("path");
+
+// Set The Storage Engine
+const storage = multer.diskStorage({
+  destination: "./public/uploads/",
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+// Init Upload
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1000000 },
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+}).single("myImage");
+
+// Check File Type
+function checkFileType(file, cb) {
+  // Allowed ext
+  const filetypes = /jpeg|jpg|png|gif/;
+  // Check ext
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  // Check mime
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb("Error: Images Only!");
+  }
+}
 
 const app = express();
-app.use(express.urlencoded({ extended: true }))
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
+  res.render("index");
+});
+
+app.post("/upload", (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      res.render("index", {
+        msg: err,
+      });
+    } else {
+      if (req.file == undefined) {
+        res.render("index", {
+          msg: "Error: No File Selected!",
+        });
+      } else {
+        res.render("index", {
+          msg: "File Uploaded!",
+          file: `uploads/${req.file.filename}`,
+        });
+      }
+    }
+  });
 });
 
 app.listen(3000, function () {
-    console.log("Server started on port 3000")
-})
+  console.log("Server started on port 3000");
+});
