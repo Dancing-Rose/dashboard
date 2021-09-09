047a417c6007c7ae3c8afc6f71c7857c927b7405
iqfareez <iqmal3@outlook.com>
2021-09-09 17:52:37 +0800

    Updated app.js

--- app.js
+++ app.js
@@ -7,18 +7,41 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.post('/requestCounterMeasure', (req, res) => {
+    console.log(req.body);
 
     let axiosConfig = {
         headers: {
-            'Authorization': 'Bearer SpVtwFtX2xKjNScdZ4kCyHVr6cPZ3BDH',
+            'Authorization': 'Bearer bBeGmd3Gi1H4f9tfH5XcnaAfwsf3yQCj',
         }
     };
 
-    axios.post("http://40.119.238.158:80/api/v1/service/automobile-endpoint/score", req.body, axiosConfig)
+    axios.post("http://20.197.106.3:80/api/v1/service/finaloutput/score", req.body, axiosConfig)
         .then((response) => {
-            var scoredLabel = response.data.Results.WebServiceOutput0[0]['Scored Labels']
-            res.send({'result': scoredLabel})
+            var scoredLabel = response.data.Results.WebServiceOutput0[0]['ControlPrediction']
+            res.set('Access-Control-Allow-Origin', '*')
+            res.send({ 'result': scoredLabel })
+
+        })
+        .catch((err) => {
+            console.log("AXIOS ERR: ", err);
+        })
+})
+
+app.post('/classifyPaddyDisease', (req, res) => {
+    console.log(req.body);
+
+    let axiosConfig = {
+        headers: {
+            "Prediction-Key": "aa23d495840d462a85d41070caf76db0",
+            "Content-Type": "application/octet-stream",
+        }
+    };
+
+    axios.post("https://umhackcustomvision-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/99577d8b-8508-450e-ae77-c6adf0fbaabb/classify/iterations/Iteration1/image", req.body, axiosConfig)
+        .then((response) => {
             
+            res.send({ 'results': response.data['predictions'] })
+
         })
         .catch((err) => {
             console.log("AXIOS ERR: ", err);
@@ -29,5 +52,6 @@ app.listen(3000, function () {
     console.log("Server started on port 3000")
     console.log("Goto http://localhost:3000")
     console.log("Test /requestCounterMeasure api on http://localhost:3000/requestCounterMeasure");
+    console.log("Test /classifyPaddyDisease api on http://localhost:3000/classifyPaddyDisease");
 })
 
